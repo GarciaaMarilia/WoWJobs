73564f18aff53628ebfba3e72b65ab7fe47e9e00
GarciaaMarilia <garciaamarilia@gmail.com>
2022-01-18 13:55:56 -0300

    Alteração name do package

--- src/Screens/Profile.jsx
+++ src/Screens/Profile.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 
 import { Typography } from '@material-ui/core';
+import Box from '@mui/material/Box'
 
+import Space from '../Style/space';
 
 export default function Profile() {
 
     return (
-        <Typography
-            variant="h5"
-            align="center"
-            fontFamily='Oswald'
-            fontSize='40px'
-            color='#363636'
-        >
-            Profile
-        </Typography>
+        <Box style={{ margin: 50 }}>
+
+            <Space size={60} />
+
+            <Typography
+                variant="h5"
+                align="center"
+                fontFamily='Oswald'
+                fontSize='40px'
+                color='#363636'
+            >
+                Profile
+            </Typography>
+        </Box>
     )
 }
